8a37fc815c7e9840f941ca39913714adf74508f6
Andres2D <andres2d1997.1206@gmail.com>
2025-03-19 11:44:49 -0400

    feat: using server queries to load data on frontend

--- 05-onwards-foodies-starting-project/app/meals/page.jsx
+++ 05-onwards-foodies-starting-project/app/meals/page.jsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
 import styles from './page.module.css';
 import MealsGrid from '../../components/meals/meals-grid';
+import { getMeals } from "../../lib/meals";
 
-export default function Meals() {
+export default async function Meals() {
+
+  const meals = await getMeals();
+
   return (
     <>
       <header className={styles.header}>
@@ -16,7 +20,7 @@ export default function Meals() {
       </header>
       <main className={styles.main}>
         <MealsGrid 
-          meals={[]}
+          meals={meals}
         />
       </main>
     </>
