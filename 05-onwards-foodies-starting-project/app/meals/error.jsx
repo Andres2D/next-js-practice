0cb91a21428ab190f974fc7bf39a31ff224b0dd1
Andres2D <andres2d1997.1206@gmail.com>
2025-03-20 11:07:38 -0400

    feat: handling error with error pages

---
+++ 05-onwards-foodies-starting-project/app/meals/error.jsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function Error({error}) {
+  console.log(error);
+  return (
+    <main className='error'>
+      <h1>An error ocurred!</h1>
+      <p>Failed to fetch meals data. Try again later.</p>
+    </main>
+  )
+}
