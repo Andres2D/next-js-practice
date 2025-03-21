4cdfb1c9c5b20875aabba7a6ed5e4408db05b008
Andres2D <andres2d1997.1206@gmail.com>
2025-03-21 11:05:45 -0400

    feat: add dependencies to generate slug text and protect agains cross browser attacks

--- 05-onwards-foodies-starting-project/lib/meals.js
+++ 05-onwards-foodies-starting-project/lib/meals.js
@@ -1,4 +1,6 @@
 import sql from 'better-sqlite3';
+import slugify from 'slugify';
+import xss from 'xss';
 
 const db = sql('meals.db');
 
@@ -12,4 +14,9 @@ export  const getMeals = async() => {
 export const getMeal = (slug) => {
   // This way protects you from sql injections
   return db.prepare('SELECT * FROM meals WHERE slug = ? ').get(slug);
+}
+
+export const saveMeal = (meal) => {
+  meal.slug = slugify(meal.title, { lower: true });
+  meal.instructions = xss(meal.instructions);
 }
