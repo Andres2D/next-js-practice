2dac52d34f24997d95ef65a0586e588e6e91a0ca
Andres2D <andres2d1997.1206@gmail.com>
2025-03-25 11:05:58 -0400

    feat: improve news list page

--- 02-starting-project/app/news/page.jsx
+++ 02-starting-project/app/news/page.jsx
@@ -1,20 +1,22 @@
 import Link from 'next/link';
+import { DUMMY_NEWS } from '../../dummy-news';
 
 export default function NewsPage() {
   return (
     <main>
-      <p>News page works!</p>
-
-      <ul>
-        <li>
-          <Link href="/news/asteroid">Asteroid</Link>
-        </li>
-        <li>
-          <Link href="/news/quantum-computing">Quantum computing</Link>
-        </li>
-        <li>
-          <Link href="/news/war">war</Link>
-        </li>
+      <ul className='news-list'>
+        {
+          DUMMY_NEWS.map((item) => {
+            return (
+              <li key={item.id}>
+                <Link href={`/news/${item.slug}`}>
+                  <img src={`/images/news/${item.image}`} alt={item.title} />
+                  <span>{item.title}</span>
+                </Link>
+              </li>
+            )
+          })
+        }
       </ul>
     </main>
   )
