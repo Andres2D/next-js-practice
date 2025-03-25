9addd227a89fe8497b74362d1a316ad9daf31050
Andres2D <andres2d1997.1206@gmail.com>
2025-03-25 11:14:59 -0400

    feat: add not found fallbacks

--- 02-starting-project/app/news/[slug]/page.jsx
+++ 02-starting-project/app/news/[slug]/page.jsx
@@ -1,7 +1,13 @@
 import { DUMMY_NEWS } from "@/dummy-news"
+import { notFound } from "next/navigation";
 
 export default function NewsDetail({params}) {
   const newsItem = DUMMY_NEWS.find(item => item.slug == params.slug);
+
+  if(!newsItem) {
+    notFound();
+  }
+
   return (
     <article className="news-article">
       <header>
