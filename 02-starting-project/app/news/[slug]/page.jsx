b24f63b1dd3c53254ebcdcce069ffa063475abfb
Andres2D <andres2d1997.1206@gmail.com>
2025-03-26 08:00:14 -0400

    feat: add new image page

--- 02-starting-project/app/news/[slug]/page.jsx
+++ 02-starting-project/app/news/[slug]/page.jsx
@@ -1,5 +1,6 @@
-import { DUMMY_NEWS } from "@/dummy-news"
 import { notFound } from "next/navigation";
+import Link from 'next/link';
+import { DUMMY_NEWS } from "@/dummy-news"
 
 export default function NewsDetail({params}) {
   const newsItem = DUMMY_NEWS.find(item => item.slug == params.slug);
@@ -11,7 +12,9 @@ export default function NewsDetail({params}) {
   return (
     <article className="news-article">
       <header>
-        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        <Link href={`/news/${newsItem.slug}/image`}>
+          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        </Link>
         <h1>{newsItem.title}</h1>
         <time dateTime={newsItem.date}>{newsItem.date}</time>
       </header>
