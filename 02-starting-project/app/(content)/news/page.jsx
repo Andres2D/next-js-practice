a97ade3d734679be331f1be7b7c987b93cd90252
Andres2D <andres2d1997.1206@gmail.com>
2025-03-28 09:48:21 -0400

    feat: add route groups feature

--- 02-starting-project/app/(content)/news/page.jsx
+++ 02-starting-project/app/(content)/news/page.jsx
@@ -1,4 +1,4 @@
-import { DUMMY_NEWS } from '../../dummy-news';
+import { DUMMY_NEWS } from '../../../dummy-news';
 import NewsList from '@/components/news-list';
 
 export default function NewsPage() {
