a036dd609fae13528a038010b7c5f1574547664c
Andres2D <andres2d1997.1206@gmail.com>
2025-04-03 11:21:01 -0400

    feat: update title and labels

--- 03-starting-project/components/form-submit.jsx
+++ 03-starting-project/components/form-submit.jsx
@@ -4,9 +4,7 @@ import { useFormStatus } from 'react-dom';
 
 export default function FormSubmit() {
   const status = useFormStatus();
-
-  console.log(status);
-
+  
   if(status.pending) {
     return <span>Creating post...</span>;
   }
