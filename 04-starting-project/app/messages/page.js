c591bd3c6439117f1510a59926259dfcc1d0b7da
Andres2D <andres2d1997.1206@gmail.com>
2025-04-07 10:15:43 -0400

    feat: new approches to control cache in components

--- 04-starting-project/app/messages/page.js
+++ 04-starting-project/app/messages/page.js
@@ -1,7 +1,13 @@
+import { unstable_noStore } from 'next/cache';
 import Messages from '@/components/messages';
 
-export default async function MessagesPage() {
+// Configure cache handling with reserved variable name
+// export const revalidate = 5;
 
+// Another way to configure cache with reserved var name
+// export const dynamic = 'force-dynamic'; // cache: 'no-store'
+
+export default async function MessagesPage() {
 
   // No cache configuration option 1
   // const response = await fetch('http://localhost:8080/messages', {
@@ -9,11 +15,16 @@ export default async function MessagesPage() {
   // });
 
   // Keep cache for 5 seconds before removing it again
-  const response = await fetch('http://localhost:8080/messages', {
-    next: {
-      revalidate: 5
-    }
-  });
+  // const response = await fetch('http://localhost:8080/messages', {
+  //   next: {
+  //     revalidate: 5
+  //   }
+  // });
+
+  // Call function to no store cache in this component
+  unstable_noStore();
+
+  const response = await fetch('http://localhost:8080/messages');
 
   const messages = await response.json();
 
