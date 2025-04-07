e561fd77dd635781111f9198e5cfa650aea425b6
Andres2D <andres2d1997.1206@gmail.com>
2025-04-07 10:09:25 -0400

    feat: control ways to manage cache

--- 04-starting-project/app/messages/page.js
+++ 04-starting-project/app/messages/page.js
@@ -1,11 +1,20 @@
 import Messages from '@/components/messages';
 
 export default async function MessagesPage() {
+
+
+  // No cache configuration option 1
+  // const response = await fetch('http://localhost:8080/messages', {
+  //   cache: 'no-store' 
+  // });
+
+  // Keep cache for 5 seconds before removing it again
   const response = await fetch('http://localhost:8080/messages', {
-    headers: {
-      'X-ID': 'page',
-    },
+    next: {
+      revalidate: 5
+    }
   });
+
   const messages = await response.json();
 
   if (!messages || messages.length === 0) {
