2025b246fae41be50ad97db1020e800ea490cda3
Andres2D <andres2d1997.1206@gmail.com>
2025-04-08 10:38:55 -0400

    feat: add dynamic metadata

--- 03-starting-project/app/feed/page.js
+++ 03-starting-project/app/feed/page.js
@@ -1,6 +1,20 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+// export const metadata = {
+//   title: 'All posts',
+//   description: 'Browse all uur posts!'
+// };
+
+export const generateMetadata = async(config) => {
+  const posts = await getPosts();
+  const numberOfPosts = posts.length;
+  return {
+    title: `(${numberOfPosts}) Posts!`,
+    description: 'Browse all our posts!'
+  }
+};
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
