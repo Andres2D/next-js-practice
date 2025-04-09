70ce73a3ed8c593aebef12ec27f81be42a450546
Andres2D <andres2d1997.1206@gmail.com>
2025-04-09 11:39:40 -0400

    feat: add server action to handle form submition and errros

---
+++ 06-starting-project/actions/auth-actions.js
@@ -0,0 +1,25 @@
+'use server';
+
+export const signUp = async(_, formData) => {
+  const email = formData.get('email');
+  const password = formData.get('password');
+
+  let errors = {};
+
+  if(!email.includes('@')) {
+    errors.email = 'Please enter a valid email address';
+  }
+
+  if(password.trim().length < 3) {
+    errors.password = 'Password must be at least 3 characters long';
+  }
+
+  if(Object.keys(errors).length > 0) {
+    return {
+      errors
+    }
+  }
+
+  return {}
+
+};
