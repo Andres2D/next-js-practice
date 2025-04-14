45983f28d1b3cc8520f1c900997d2f6cabbfe2e6
Andres2D <andres2d1997.1206@gmail.com>
2025-04-14 11:18:25 -0400

    feat: use more nested routes

---
+++ 07-old-starting-project/pages/clients/[id]/[clientProjectId].jsx
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router';
+
+export default function SelectedClientProjectPage() {
+  
+  const router = useRouter();
+
+  console.log(router);
+  
+  return (
+    <div>
+      <h1>The project Page for a Specific Project for a Selected Client</h1>
+    </div>
+  )
+}
